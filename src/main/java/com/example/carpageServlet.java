0907af86380d7affde41cc639b80cebd3a06e59a
Charan-Samsani <charan.blend@gmail.com>
2023-10-26 22:40:31 +0530

    edited pom and added src folder

---
+++ src/main/java/com/example/carpageServlet.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/carpage")
+public class carpageServlet extends HttpServlet {
+
+	    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		            request.getRequestDispatcher("/index.html").forward(request, response);
+			        }
+}
+
