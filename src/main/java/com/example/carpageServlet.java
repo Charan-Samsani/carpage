e09132966e363ec20d7fa3aff6e85efe39c6ac91
Charan <charansamsani.official@gmail.com>
2023-10-26 22:49:59 +0530

    Update carpageServlet.java

--- src/main/java/com/example/carpageServlet.java
+++ src/main/java/com/example/carpageServlet.java
@@ -11,8 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/carpage")
 public class carpageServlet extends HttpServlet {
 
-	    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		            request.getRequestDispatcher("/index.html").forward(request, response);
-			        }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	request.getRequestDispatcher("/index.html").forward(request, response);
+	}
 }
 
